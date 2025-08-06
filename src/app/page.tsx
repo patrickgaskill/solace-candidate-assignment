cb119a2b0f49a13b3fd1f74605aa4713f617d408
Patrick Gaskill <29219+patrickgaskill@users.noreply.github.com>
2025-08-05 20:34:22 -0400

    fix missing key props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,14 +93,14 @@ export default function Home() {
         <tbody>
           {filteredAdvocates.map((advocate) => {
             return (
-              <tr>
+              <tr key={advocate.id}>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
                 <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
+                  {advocate.specialties.map((s, i) => (
+                    <div key={i}>{s}</div>
                   ))}
                 </td>
                 <td>{advocate.yearsOfExperience}</td>
